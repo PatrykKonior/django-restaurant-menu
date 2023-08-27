e7234402ffdec6c3cf02dacec0938412f5ff17cb
Patryk <patrykkonior4@gmail.com>
2023-08-27 17:44:08 +0200

    Implement class-based views

--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Item
 
+
 class MenuList(generic.ListView):
-    queryset = Item.object.order_by("date_created")
+    queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
 
 
